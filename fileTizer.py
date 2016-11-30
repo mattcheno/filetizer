b94561a2b57f6f7f74f5d6713ea85791e65121bd
mattcheno <matthew.william.chenoweth@gmail.com>
2016-11-30 14:03:24 -0600

    Exclude Hidden Files and Directories

--- fileTizer.py
+++ fileTizer.py
@@ -7,27 +7,31 @@
 #  2016/11/30
 
 # --- Declarations ------------------------------------------------------------
-import os, shutil, re
+import os, shutil#, re
 
 # --- Logic -------------------------------------------------------------------
 print('Enter directory location')
 tarDir = input()
 outFile = open('output.txt', 'w')
-regxIgnore = re.compile(r'^[.]', re.I)
+#regxIgnore = re.compile(r'^[.]', re.I)
 
 #TODO: Check if directory exists, throw error and stop script if not
 
 outFile.write('Target directory: ' + tarDir + '\n')
-for dir, subdirs, fileNames in os.walk(tarDir):
-	dirMat = regxIgnore.search(dir)
-	outFile.write('Current directory: ' + dirmat.group() + '\n')
+for dirs, subdirs, files in os.walk(tarDir):
+	#dirMat = regxIgnore.search(dir)
+	files = [f for f in files if not f[0] == '.']
+	subdirs[:] = [s for s in subdirs if not s[0] == '.']
+	#dirs = [d for d in dirs if not d[0] == '.']
+	
+	outFile.write('Current directory: ' + dirs + '\n')
 	
 #	for dirName in subdirs:
-#		outFile.write('Sub-directory of ' + dir + ': ' + dirName + '\n')
+#		outFile.write('Sub-directory of ' + dirs + ': ' + subdirs + '\n')
 	
-	for fiName in fileNames:
-		fiMat = regxIgnore.search(fiName)
-		outFile.write('File in ' + dir + ': ' + fiMat.group() + '\n')
+	for fiName in files:
+		#fiMat = regxIgnore.search(fiName)
+		outFile.write('File in ' + dirs + ': ' + fiName + '\n')
 	
 	outFile.write('::  \n')
 
